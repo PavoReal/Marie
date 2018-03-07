dcdf27310a71dad4e8a85ddffabfcf661e977982
Garrison Peacock <garrison.peacock@gmail.com>
2018-03-07 15:55:44 -0800

    Linux build changes

--- build.sh
+++ build.sh
@@ -8,9 +8,12 @@ mkdir -p build/
 
 pushd build/ > /dev/null
 
+echo -e "\e[7masm_marie.cpp...\e[0m"
 clang++ $CPP_FLAGS ../src/asm_marie.cpp -o $PRODUCT_ASM
-clang++ $CPP_FLAGS ../src/exe_marie.cpp -o $PRODUCT_EXE
 
+echo -e "\e[7mexe_marie.cpp...\e[0m"
+clang++ $CPP_FLAGS ../src/exe_marie.cpp -o $PRODUCT_EXE
+echo -e "\e[7m\ncompilation done...\e[0m"
 cp ../src/*.mar ./
 
 popd > /dev/null
